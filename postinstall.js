e1c321645e2d76ca447ad7abe587439ead7eab1c
Giesenberg, Jan <jan.giesenberg@sybit.de>
2020-05-28 17:15:52 +0200

    fixed installation procedure

--- postinstall.js
+++ postinstall.js
@@ -1,38 +1,39 @@
-var path = require('path');
-var fs = require('fs');
+var path = require('path')
+var fs = require('fs')
 var packageJson = require('./package.json')
 
 var pluginName = packageJson['@pattern-lab-plugin'].name
 var pluginModuleName = packageJson.name
-var node_modules_path = path.join(process.cwd(), 'node_modules');
+var nodeModulesPath = path.join(process.cwd(), '..', '..')
 
-function copyFolderRecursiveSync( source, target ) {
-  var files = [];
+function copyFolderRecursiveSync (source, target) {
+  var files = []
 
-  //check if folder needs to be created or integrated
-  var targetFolder = path.join( target, path.basename( source ) );
-  if ( !fs.existsSync( targetFolder ) ) {
-      fs.mkdirSync( targetFolder, {
-        recursive: true
-      } );
+  // check if folder needs to be created or integrated
+  var targetFolder = path.join(target, path.basename(source))
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder, {
+      recursive: true
+    })
   }
 
-  //copy
-  if ( fs.lstatSync( source ).isDirectory() ) {
-      files = fs.readdirSync( source );
-      files.forEach( function ( file ) {
-          var curSource = path.join( source, file );
-          if ( fs.lstatSync( curSource ).isDirectory() ) {
-              copyFolderRecursiveSync( curSource, targetFolder );
-          } else {
-              fs.copyFileSync( curSource, path.join(targetFolder, file) );
-          }
-      } );
+  // copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source)
+    files.forEach(function (file) {
+      var curSource = path.join(source, file)
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder)
+      } else {
+        fs.copyFileSync(curSource, path.join(targetFolder, file))
+      }
+    })
   }
 }
 
-if (process.env.PATTERLAB_PLUGIN_DEV !== pluginModuleName) {
-  copyFolderRecursiveSync(path.join(node_modules_path, pluginModuleName), path.join(node_modules_path));
+var parentPackageJson = path.resolve(nodeModulesPath, '..', 'package.json')
+if (fs.existsSync(parentPackageJson) && require(parentPackageJson).name !== pluginModuleName) {
+  copyFolderRecursiveSync(path.join(nodeModulesPath, pluginModuleName), path.join(nodeModulesPath))
 
   console.log(`Pattern Lab Node Plugin - ${pluginModuleName} installed as ${pluginName}. `)
   console.log('You may have to run `npm run postinstall` from your console as well.')
